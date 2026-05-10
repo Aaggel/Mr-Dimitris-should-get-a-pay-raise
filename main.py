91cfddd9febac3a2b8adb751b95160dab8e01253
Angelos <110755825+Aaggel@users.noreply.github.com>
2026-05-10 18:40:32 +0300

    Update main.py
    ohmygod

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def train_and_evaluate(model, x_train, x_test, y_train, y_test, epochs, batch_si
     print("Accuracy:", accuracy)
     print("Confusion Matrix:")
     print(cm)
-    print("\n    /)/)\n( ˶•༝•)\n୭( づ✿")
+    print("\n /)/)\n( ˶•༝•)\n୭( づ✿")
     # για να κρατάει τις καλύτερες περιπτώσεις / to keep the best cases
     if title == "CASE1":
         results_case1.append((cm, accuracy))
